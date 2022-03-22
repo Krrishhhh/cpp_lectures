b2031a5b47a90c7c87f73e6fa53214149c2e6896
Krrishhhh <singh.krish.2001@gmail.com>
2022-03-22 11:49:53 +0000

    Day 1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,29 @@
-#include <cstdio>
 
-int main() { std::printf("%s\n", "Hello World"); }
+#include <iostream> //cout; cin
+#include <string>  // string
+
+int main() 
+{
+        //{1} testing gitpod
+        // std::cout << "test message" << std:: endl;
+        std::cout << "test" << std::endl;
+
+        //{2} testing address
+        int year {2022}; // declaring a variable
+        std::cout << "Variable value: " << year << std::endl;
+        std::cout << "Variable address: " << &year << std::endl;
+
+        //{3}
+        std::string msg {"Hello World!"};
+        std::string* p_msg {&msg};
+        std::cout << "Variable value: " << msg << std::endl;
+        std::cout << "Variable address: " << &msg << std::endl;
+        std::cout << "pointer value: " << p_msg << std::endl;
+
+        //{4} Excersice
+        std::string saby {"Where am I!"};
+        std::string* pointter {&saby};
+        
+
+}
+
