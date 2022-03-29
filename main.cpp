efd5b7f656320ad5805fb305711a058b20256ef3
Krrishhhh <singh.krish.2001@gmail.com>
2022-03-29 14:51:54 +0000

    no 1

--- main.cpp
+++ main.cpp
@@ -1,29 +1,19 @@
 
 #include <iostream> //cout; cin
 #include <string>  // string
+#include <vector>
 
-int main() 
+auto get_two_ints(int& x)
+auto compute_addition(float x, float y) -> float {return x + y; }
+auto compute_subtraction(float x, float y) -> float {return x - y; }
+auto compute_multiply(float x, float y) -> float {return x * y; }
+auto compute_division(float x, float y) -> float {return x / y; }
+
+using BinaryFn = float (*)(float, float);
+
+auto main()-> int
 {
-        //{1} testing gitpod
-        // std::cout << "test message" << std:: endl;
-        std::cout << "test" << std::endl;
-
-        //{2} testing address
-        int year {2022}; // declaring a variable
-        std::cout << "Variable value: " << year << std::endl;
-        std::cout << "Variable address: " << &year << std::endl;
-
-        //{3}
-        std::string msg {"Hello World!"};
-        std::string* p_msg {&msg};
-        std::cout << "Variable value: " << msg << std::endl;
-        std::cout << "Variable address: " << &msg << std::endl;
-        std::cout << "pointer value: " << p_msg << std::endl;
-
-        //{4} Excersice
-        std::string saby {"Where am I!"};
-        std::string* pointter {&saby};
-        
-
+        std::vector<BinaryFn> functions {};
+        functions.push_back(compute_subtraction);hlih
+        functions.push_back(compute_addition);
 }
-
